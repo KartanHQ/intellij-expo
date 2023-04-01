5c05da54956cc41ccb94a1a0573e81ae0b6cbd23
Milad Nekofar <milad@nekofar.com>
2023-04-01 23:19:34 +0330

    refactor: update plugin icon and add extra icons

---
+++ src/main/kotlin/com/nekofar/milad/intellij/expo/ExpoIcons.kt
@@ -0,0 +1,11 @@
+package com.nekofar.milad.intellij.expo
+
+import com.intellij.openapi.util.IconLoader
+
+object ExpoIcons {
+    @JvmField
+    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
+
+    @JvmField
+    val ProjectGenerator = IconLoader.getIcon("/icons/expo.svg", javaClass)
+}
