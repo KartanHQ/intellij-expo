5c1a0008204b8764d78261d97eea887a71b2312c
Milad Nekofar <milad@nekofar.com>
2023-06-30 07:04:15 +0330

    feat: add TypeScript support to Expo project generator
    This commit adds TypeScript support to the Expo CLI project generator in order to allow the user to generate an Expo project with TypeScript enabled.
    
    https://github.com/KartanHQ/intellij-expo/issues/30

--- src/main/kotlin/com/nekofar/milad/intellij/expo/cli/ExpoCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/expo/cli/ExpoCliProjectGenerator.kt
@@ -1,18 +1,27 @@
 package com.nekofar.milad.intellij.expo.cli
 
 import com.intellij.execution.filters.Filter
+import com.intellij.ide.util.projectWizard.SettingsStep
 import com.intellij.lang.javascript.boilerplate.NpmPackageProjectGenerator
 import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.NlsSafe
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.ProjectGeneratorPeer
+import com.intellij.ui.components.JBCheckBox
 import com.nekofar.milad.intellij.expo.ExpoBundle
 import com.nekofar.milad.intellij.expo.ExpoIcons
 import javax.swing.Icon
+import javax.swing.JPanel
 
 class ExpoCliProjectGenerator : NpmPackageProjectGenerator() {
     private val packageName = "create-expo-app"
     private val npxCommand = "create-expo-app"
+
+    private val typeScriptKey = Key.create<Boolean>("expo.project.generator.typescript.project")
+    private val typeScriptInitial = false
 
     override fun getName(): String = ExpoBundle.message("expo.project.generator.name")
 
@@ -22,6 +31,35 @@ class ExpoCliProjectGenerator : NpmPackageProjectGenerator() {
 
     override fun customizeModule(virtualFile: VirtualFile, contentEntry: ContentEntry?) {}
 
+    override fun createPeer(): ProjectGeneratorPeer<Settings> {
+        val typeScriptCheckbox = JBCheckBox(
+            ExpoBundle.message("expo.project.generator.typescript.checkbox"),
+            typeScriptInitial
+        )
+
+        return object : NpmPackageGeneratorPeer() {
+            override fun buildUI(settingsStep: SettingsStep) {
+                super.buildUI(settingsStep)
+                settingsStep.addSettingsComponent(typeScriptCheckbox)
+            }
+
+            override fun getSettings(): Settings {
+                val settings = super.getSettings()
+                settings.putUserData(
+                    typeScriptKey,
+                    typeScriptCheckbox.isSelected
+                )
+                return settings
+            }
+
+            override fun createPanel(): JPanel {
+                val panel = super.createPanel()
+                panel.add(typeScriptCheckbox)
+                return panel
+            }
+        }
+    }
+
     override fun packageName(): String = packageName
 
     override fun presentablePackageName(): String =
@@ -29,10 +67,18 @@ class ExpoCliProjectGenerator : NpmPackageProjectGenerator() {
 
     override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageName, npxCommand))
 
-    override fun generateInTemp(): Boolean = false
+    override fun generateInTemp(): Boolean = true
 
-    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> =
-        arrayOf(".")
+    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<@NlsSafe String>? {
+        val typeScript = settings?.getUserData(typeScriptKey) ?: typeScriptInitial
+        return project?.let {
+            if (typeScript) arrayOf(
+                "-t",
+                "expo-template-blank-typescript",
+                it.name
+            ) else arrayOf(it.name)
+        }
+    }
 
     override fun getIcon(): Icon = ExpoIcons.ProjectGenerator
 }
